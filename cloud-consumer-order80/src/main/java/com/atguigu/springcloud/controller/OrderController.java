aaf89fe2cf550a7a1069feb8de112e6fa8042f18
wugm <wugm@bsfit.com.cn>
2022-03-29 21:09:37 +0800

    修复接口

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.atguigu.springcloud.controller;
 
-
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
@@ -9,29 +8,32 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
-
+//远程模块调用示例
 @RestController
 @Slf4j
 public class OrderController {
 
-    //本地8001提供服务地址
-    public static final String PAYMENT_URL = "http://localhost:8081";
+    //本地8001提供服务的地址
+    public static final String PAYMENT_URL="http://localhost:8081";
+
 
     @Resource
     private RestTemplate restTemplate;
 
-    //调用服务端create方法
     @PostMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment){
+    public CommonResult<Payment> create(@RequestBody Payment payment){
+
         //返回远程调用结果
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
+
     }
 
-    @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> get(@PathVariable("id") Long id){
-        System.out.println("我执行了");
+
+    @GetMapping("/consumer/payment/get")
+    public CommonResult<Payment> get(@RequestParam("id") Long id){
+
+        System.out.println(id);
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get?id="+id,CommonResult.class);
+
     }
-
-
 }
